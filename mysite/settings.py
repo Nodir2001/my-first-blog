73867a2924cd8a32343855d96f60743936942577
Nodir2001 <ras.nodir@mail.ru>
2020-08-03 16:37:36 +0300

    changed Degub=True

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j*+@(&5!yuj9txmdtfr0k38)vo51ruttwyt6ad)nl&rmkh7!k5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
